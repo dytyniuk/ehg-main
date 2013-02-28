c957ba7543a3db6b288965e502c97dad56464eca
Andrew Kushch <andrii.kushch@gmail.com>
2013-02-28 08:42:51 +0200

    translation modified

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -113,7 +113,7 @@
         	<div class="row-fluid">
                 <div class="span2 brief">
                     <?php echo Yii::t('main-ui', 'Download our brief!'); ?><br/>
-                    <a href="http://ehgroup.com.ua/brif.doc" class="btn btn-warning"><?php echo Yii::t('main-ui', 'Download'); ?></a>
+                    <a href="<?php echo Yii::app()->request->baseUrl; ?>/css/Brif.doc" class="btn btn-warning"><?php echo Yii::t('main-ui', 'Download'); ?></a>
                 </div>
                 <div class="span3 news">
                     <h4><?php echo Yii::t('main-ui', 'Latest News'); ?></h4>
